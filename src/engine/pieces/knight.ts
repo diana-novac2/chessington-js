001aa6ee3d4690afd76070f90da8125019234525
Diana Novac <Diana.Novac@softwire.com>
2025-07-07 15:23:53 +0300

    Added knight movement logic

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,25 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const verticalJump = [1, 1, 2, 2, -1, -1, -2, -2];
+        const horizontalJump = [2, -2, 1, -1, 2, -2, -1, 1];
+
+        const currentPosition = board.findPiece(this);
+        let moves: Square[] = [];
+
+        for (let i = 0; i < 8; i++) {
+            const nextPosition = new Square(currentPosition.row + verticalJump[i],
+                                            currentPosition.col + horizontalJump[i]);
+            if (this.isValidMove(nextPosition)) {
+                moves.push(nextPosition);
+            }
+        }
+
+        return moves;
     }
+
+    private isValidMove(pos: Square) {
+        return pos.row >= 0 && pos.col >= 0 && pos.row < 8 && pos.col < 8;
+    }
+
 }
