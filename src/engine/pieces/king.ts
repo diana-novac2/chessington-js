3b9243c6f04bd973423f106f4cf1d699600fd225
Diana Novac <Diana.Novac@softwire.com>
2025-07-07 15:50:18 +0300

    Implemented king movement logic

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,22 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentPosition = board.findPiece(this);
+        const verticalMoves = [-1, 0, 1];
+        const horizontalMoves = [-1, 0, 1];
+
+        let moves: Square[] = [];
+
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                const nextPosition = new Square(currentPosition.row + verticalMoves[row],
+                                                currentPosition.col + horizontalMoves[col]);
+                if (nextPosition.isInsideBoard() && !nextPosition.equals(currentPosition)) {
+                    moves.push(nextPosition);
+                }
+            }
+        }
+
+        return moves;
     }
 }
