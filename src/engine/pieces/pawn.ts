5e35d9aa94a08bc8cf0fd7d16e918ea4e3b9f07f
Diana Novac <Diana.Novac@softwire.com>
2025-07-07 13:31:46 +0300

    Made pawns move one or two spaces

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -4,15 +4,48 @@ import Board from '../board';
 import Square from "../square";
 
 export default class Pawn extends Piece {
+    private firstMove: boolean;
+
     public constructor(player: Player) {
         super(player);
+        this.firstMove = true;
+    }
+
+
+    public moveTo(board: Board, newSquare: Square) {
+        this.firstMove = false;
+        super.moveTo(board, newSquare);
     }
 
     public getAvailableMoves(board: Board) {
         const currentPosition = board.findPiece(this);
-        const upOneSquare = new Square(currentPosition.row - 1, currentPosition.col);
-        const downOneSquare = new Square(currentPosition.row + 1, currentPosition.col);
 
-        return [upOneSquare, downOneSquare];
+        if (this.player === Player.WHITE) {
+            return this.whitePawnMoves(currentPosition);
+        } else {
+            return this.blackPawnMoves(currentPosition);
+        }
+    }
+
+    private whitePawnMoves(currentPosition: Square) {
+        const upOneSquare = new Square(currentPosition.row + 1, currentPosition.col);
+        const upTwoSquares = new Square(currentPosition.row + 2, currentPosition.col);
+
+        if (this.firstMove) {
+            return [upOneSquare, upTwoSquares];
+        }
+
+        return [upOneSquare];
+    }
+
+    private blackPawnMoves(currentPosition: Square) {
+        const downOneSquare = new Square(currentPosition.row - 1, currentPosition.col);
+        const downTwoSquares = new Square(currentPosition.row - 2, currentPosition.col);
+
+        if (this.firstMove) {
+            return [downOneSquare, downTwoSquares];
+        }
+
+        return [downOneSquare];
     }
 }
